cf558ef0f15bde49d5883de7e0a5c164e3b61bb0
RORO <RORO@jgmail.com>
2019-03-29 14:57:25 +0100

    memory-done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'memory-game', pathMatch: 'full' },
+  { path: '', redirectTo: 'main', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
   { path: 'animals', loadChildren: './pages/animals/animals.module#AnimalsPageModule' },
   { path: 'main', loadChildren: './pages/main/main.module#MainPageModule' },
--- src/app/memory-game/memory-game.page.ts
+++ src/app/memory-game/memory-game.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-memory-game',
   templateUrl: './memory-game.page.html',
@@ -13,16 +14,76 @@ export class MemoryGamePage implements OnInit {
 
   private questionMarkUrl = "/assets/images/question-mark.png";
 
+  private hasARevealedCard: boolean = false;
+
+  private previousCard;
+
+  /**
+   * Generation du jeux de cartes
+   */
   private generateDeck() {
     for (let i = 0; i < this.numberOfDistinctCards; i++) {
-      let imgUrl = "/assets/img/cards/" + i + ".png";
+      let imgUrl = "/assets/images/cards/" + i + ".png";
       this.cardList.push({ img: this.questionMarkUrl, name: imgUrl, revealed: false });
       this.cardList.push({ img: this.questionMarkUrl, name: imgUrl, revealed: false });
     }
+
   }
+  /**
+   * Mélange des cartes
+   */
+  private shuffleDeck() {
+    //Boucle dans le tableau et melange les cartes 
+    this.cardList.forEach(
+      (item, index, deck) => {
+        //position aléatoire dans le tableau
+        let newPos = Math.floor(Math.random() * deck.length);
+        //permutation
+        deck[index] = deck[newPos];
+        deck[newPos] = item;
+      })
+  }
+  /**
+   * Fonction de retournement de la carte
+   * @param card 
+   * @param index 
+   */
+  private flipCard(card, index) {
+    //Affichage de la carte si elle n'est pas déjà révélé et si aucune autre carte n'est en cour d'affcihage
+    if (!card.revealed && !this.hasARevealedCard) {
+
+      //Affichage de la carte
+      card.img = card.name;
+      card.revealed = true;
+      this.hasARevealedCard = true;
+
+
+      if (this.previousCard && this.previousCard.name == card.name) {
+        this.previousCard.img = card.name;
+        this.previousCard.revealed = true;
+        this.hasARevealedCard = false;
+      } else {
+        //Masquage de la carte au bout de 1 seconde
+        setTimeout(
+          () => {
+            card.img = this.questionMarkUrl;
+            card.revealed = false;
+            this.hasARevealedCard = false;
+            this.previousCard = card;
+          },
+          1000
+        )
+      }
+
+
+    }
+
+  }
+
   constructor() {
     this.generateDeck();
-    console.log(this.cardList);
+    this.shuffleDeck();
+
   }
 
   ngOnInit() {
